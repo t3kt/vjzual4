f7799b59ca7e181abfbf8a76d63d8f1a30d5606a
tekt <tommy.etkin@gmail.com>
2018-06-19 21:41:07 -0700

    starting to update the app host
    see #12, #9

--- lib/app_host.py
+++ lib/app_host.py
@@ -21,6 +21,11 @@ try:
 except ImportError:
 	schema = mod.schema
 
+try:
+	import remote_client
+except ImportError:
+	remote_client = mod.remote_client
+
 class AppHost(common.ExtensionBase, common.ActionsExt, schema.SchemaProvider):
 	def __init__(self, ownerComp):
 		common.ExtensionBase.__init__(self, ownerComp)
@@ -28,10 +33,11 @@ class AppHost(common.ExtensionBase, common.ActionsExt, schema.SchemaProvider):
 			'Attachapp': self.AttachToApp,
 		})
 		self._AutoInitActionParams()
-		self.AppRoot = None
 		self.SubModules = []
-		self.ownerComp.op('deferred_attach_app').run(delayFrames=1)
 		self.AppSchema = None  # type: schema.AppSchema
+
+	def RemoteClient(self) -> remote_client.RemoteClient:
+		return self.ownerComp.par.Remoteclient.eval()
 
 	def GetAppSchema(self):
 		return self.AppSchema
@@ -44,24 +50,7 @@ class AppHost(common.ExtensionBase, common.ActionsExt, schema.SchemaProvider):
 			o.destroy()
 
 	def AttachToApp(self):
-		self.AppRoot = self.ownerComp.par.Approot.eval()
-		self._LoadSubModules()
-		hostcore = self.ownerComp.op('host_core')
-		self._BuildSubModuleTable(hostcore.op('set_sub_module_table'))
-		# self._BuildSubModuleHosts()
-
-	def _LoadSubModules(self):
-		self.SubModules = module_host.FindSubModules(self.AppRoot)
-
-	def _BuildSubModuleTable(self, dat):
-		dat.clear()
-		dat.appendRow([
-			'name',
-			'path',
-			'label',
-		])
-		for m in self.SubModules:
-			dat.appendRow([m.name, m.path, getattr(m.par, 'Uilabel') or m.name])
+		pass
 
 	@property
 	def _SubModuleHosts(self) -> List[module_host.ModuleHostBase]:
@@ -78,21 +67,21 @@ class AppHost(common.ExtensionBase, common.ActionsExt, schema.SchemaProvider):
 		dest = self.ownerComp.op('modules_panel')
 		for m in dest.ops('mod__*'):
 			m.destroy()
-		if not self.AppRoot:
-			return
-		template = self._ModuleHostTemplate
-		if not template:
-			return
-		hosts = []
-		for i, submod in enumerate(self.SubModules):
-			host = dest.copy(template, name='mod__' + submod.name)
-			host.par.Uibuilder.expr = 'parent.AppHost.par.Uibuilder or ""'
-			host.par.Module = submod.path
-			host.par.hmode = 'fixed'
-			host.par.vmode = 'fill'
-			host.par.alignorder = i
-			host.nodeX = 100
-			host.nodeY = -100 * i
-			hosts.append(host)
-			host.AttachToModule()
+		# if not self.AppRoot:
+		# 	return
+		# template = self._ModuleHostTemplate
+		# if not template:
+		# 	return
+		# hosts = []
+		# for i, submod in enumerate(self.SubModules):
+		# 	host = dest.copy(template, name='mod__' + submod.name)
+		# 	host.par.Uibuilder.expr = 'parent.AppHost.par.Uibuilder or ""'
+		# 	host.par.Module = submod.path
+		# 	host.par.hmode = 'fixed'
+		# 	host.par.vmode = 'fill'
+		# 	host.par.alignorder = i
+		# 	host.nodeX = 100
+		# 	host.nodeY = -100 * i
+		# 	hosts.append(host)
+		# 	host.AttachToModule()
 
